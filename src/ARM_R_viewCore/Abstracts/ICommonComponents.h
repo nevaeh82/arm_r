fd0c4b33379c5482210be0c670fa057905adda61
Ivan Epifanov <iepifanov@stc-spb.ru>
2014-02-19 13:52:49 +0000

    git-svn-id: https://svn.dfpost.ru/svn/skyboard/Zaviruha4/branches/5723@2874 b71824a2-2657-4908-b26c-4c50f5581b7d

--- src/ARM_R_viewCore/Abstracts/ICommonComponents.h
+++ src/ARM_R_viewCore/Abstracts/ICommonComponents.h
@@ -9,12 +9,9 @@ class ICommonComponents
 {
 public:
 	virtual ~ICommonComponents(){}
-	//virtual int init(QString path_to_ini_file)					= 0;
 	virtual int init(int count)									= 0;
-	//virtual int set(int id, ICorrelationWidget* widget)			= 0;
 	virtual ICorrelationWidget *get(int id)						= 0;
 	virtual int count()									= 0;
-	//virtual QMap<int, ICorrelationWidget* >* get_components()   = 0;
 };
 
 #endif // ICOMMONCOMPONENTS_H
