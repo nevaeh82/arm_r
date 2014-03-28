565c4ed132ee2b711c8c9484a5932e0c9cde07be
Ivan Epifanov <iepifanov@stc-spb.ru>
2014-03-28 10:07:26 +0000

    git-svn-id: https://svn.dfpost.ru/svn/skyboard/Zaviruha4/branches/5998ZVODb@3600 b71824a2-2657-4908-b26c-4c50f5581b7d

--- src/ARM_R_ServerCore/Flakon/CoordinateCounter.cpp
+++ src/ARM_R_ServerCore/Flakon/CoordinateCounter.cpp
@@ -71,13 +71,17 @@ void CoordinateCounter::sendData(const MessageSP message)
 
 	if (messageType == TCP_FLAKON_COORDINATES_COUNTER_REQUEST_SET_SOLVER) {
 		QByteArray messageData = message->data();
-		QDataStream dataStream(&messageData, QIODevice::ReadOnly);
+		Zaviruha::Packet packet;
+		if (!packet.ParseFromArray(messageData, messageData.size())){
+			return;
+		}
 
-		int id;
-		double alt;
-		int track_length = 0;
+		//QDataStream dataStream(&messageData, QIODevice::ReadOnly);
+		//dataStream >> id >> track_length >> alt;
 
-		dataStream >> id >> track_length >> alt;
+		int id = packet.command().arguments().solverdata(0).id();
+		double alt = packet.command().arguments().solverdata(0).altitude();
+		int track_length = packet.command().arguments().solverdata(0).tracklength();
 
 		m_solver->SetHeighApriori(alt);
 		m_alt = alt;
