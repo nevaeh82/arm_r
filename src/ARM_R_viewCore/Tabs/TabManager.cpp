f203f3044a8e50282d2c0ee6051709a1984edc32
Pavel Yarovoy <pyarovoy@stc-spb.ru>
2014-04-17 06:29:51 +0000

    Move server initialization
    git-svn-id: https://svn.dfpost.ru/svn/skyboard/Zaviruha4/branches/6281@4254 b71824a2-2657-4908-b26c-4c50f5581b7d

--- src/ARM_R_viewCore/Tabs/TabManager.cpp
+++ src/ARM_R_viewCore/Tabs/TabManager.cpp
@@ -130,9 +130,6 @@ void TabManager::setAtlantConfiguration(const AtlantConfiguration& atlantConfig)
 
 void TabManager::addStationTabs()
 {
-	if (m_rpcFlakonClient != NULL) {
-		m_rpcFlakonClient->init(m_rpcPort, QHostAddress(m_rpcHost));
-	}
 
 	m_correlationControllers = new CorrelationControllersContainer(this);
 	m_correlationControllers->init(m_stationsMap.count());
@@ -147,6 +144,9 @@ void TabManager::addStationTabs()
 
 	m_rpcFlakonClient->registerReceiver(commonTabSpectrumWidget);
 
+	if (m_rpcFlakonClient != NULL) {
+		m_rpcFlakonClient->init(m_rpcPort, QHostAddress(m_rpcHost));
+	}
 	QTabBar* tabBar = m_tabWidget->findChild<QTabBar *>(QLatin1String("qt_tabwidget_tabbar"));
 
 	foreach (Station* station, m_stationsMap) {
