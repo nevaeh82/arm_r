0050d79f84671c62746004f5dc059b687c558525
pyarovoy <pyarovoy@b71824a2-2657-4908-b26c-4c50f5581b7d>
2014-05-27 14:23:53 +0000

    ------------------------------------------------------------------------ r5039 | pyarovoy | 2014-05-27 18:07:48 +0400 ('в, 27 ┐ c 2014) | 1 line
    Remove scaling graph view
    ------------------------------------------------------------------------
    r5038 | pyarovoy | 2014-05-27 18:07:15 +0400 ('в, 27 ┐ c 2014) | 1 line
    
    create branch https://svn.dfpost.ru/svn/skyboard/Zaviruha4/branches/6577
    ------------------------------------------------------------------------
    
    git-svn-id: https://svn.dfpost.ru/svn/skyboard/Zaviruha4/trunk@5040 b71824a2-2657-4908-b26c-4c50f5581b7d

--- src/ARM_R_viewCore/SpectrumWidgetController.cpp
+++ src/ARM_R_viewCore/SpectrumWidgetController.cpp
@@ -248,16 +248,7 @@ void SpectrumWidgetController::setSignal(float *spectrum, float *spectrum_peak_h
 		m_rett = -100;
 	}
 
-
 	m_graphicsWidget->PermanentDataSetup(spectrum, spectrum_peak_hold, minv, maxv);
-
-	double startHZ, endHZ;
-	double startY, endY;
-	m_graphicsWidget->GetCurrentViewport(startHZ, startY, endHZ, endY);
-	//Control chart out of viewPort - update scale
-	if( *spectrum > startY || *spectrum < endY ) {
-		m_graphicsWidget->SetScaleY_1to1();
-	}
 }
 
 void SpectrumWidgetController::setDefModulation(QString modulation)
