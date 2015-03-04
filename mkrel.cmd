c93b1d12d3485b7d83ae74b8ddabee60ba9f3bab
Kirill Sudarushkin <ksudarushkin@stc-spb.ru>
2015-03-04 17:12:33 +0300

    #Zav-20 Fixes for mkrel call errors

--- mkrel.cmd
+++ mkrel.cmd
@@ -2,16 +2,39 @@
 
 if "%PROCESSOR_ARCHITECTURE%" == "x86" ( call "c:\Program Files\Microsoft Visual Studio 10.0\vc\bin\vcvars32.bat" \
 ) else ( call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\vc\bin\vcvars32.bat" )
+if not errorlevel 0 goto error
 
 cmake -E remove_directory build
+if not errorlevel 0 goto error
+
 cmake -E make_directory  build
+if not errorlevel 0 goto error
 
 cd build
+if not errorlevel 0 goto error
 
 cmake -G "NMake Makefiles" .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=tmprel -DVERSION=%1
+if not errorlevel 0 goto error2
 
 jom all
-ctest -V -T Test
-cpack
+if not errorlevel 0 goto error2
 
+ctest -V -T Test
+if not errorlevel 0 goto error2
+
+cpack
+if not errorlevel 0 goto error2
+
+goto no_error
+
+:error
+exit /B %errorlevel%
+
+:error2
+set errorCode=%errorlevel%
+cd ..
+exit /B %errorCode%
+
+:no_error
+cd ..
 exit /B 0
