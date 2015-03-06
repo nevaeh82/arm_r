08959c1d396a841e73d0378a609e85c1319e34c3
Kirill Sudarushkin <ksudarushkin@stc-spb.ru>
2015-03-06 17:36:22 +0300

    #Zav-20 Fixed DB create scripts

--- sql/create_db.cmd
+++ sql/create_db.cmd
@@ -1,4 +1,4 @@
 @echo off
 
-mysql.exe -u%1 -p%2 --default-character-set=utf8 < arm_r_tables.sql
-mysql.exe -u%1 -p%2 --default-character-set=utf8 --database=stations < stations.sql
+mysql.exe -u%1 -p%2 --default-character-set=utf8 < %~dp0\arm_r_tables.sql
+mysql.exe -u%1 -p%2 --default-character-set=utf8 --database=stations < %~dp0\stations.sql
--- sql/create_test_db.cmd
+++ sql/create_test_db.cmd
@@ -1,4 +1,4 @@
 @echo off
 
-mysql.exe -u%1 -p%2 --default-character-set=utf8 < arm_r_tablesTest.sql
-mysql.exe -u%1 -p%2 --default-character-set=utf8 --database=testStations < stations.sql
+mysql.exe -u%1 -p%2 --default-character-set=utf8 < %~dp0\arm_r_tablesTest.sql
+mysql.exe -u%1 -p%2 --default-character-set=utf8 --database=testStations < %~dp0\stations.sql
