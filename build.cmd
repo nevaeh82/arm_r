a93f3de0f27fb5e569f26a5cadcc0cbce0ea67bd
Kirill Sudarushkin <ksudarushkin@stc-spb.ru>
2015-03-06 13:31:57 +0300

    Fixed and improved build scripts (.cmd)

--- build.cmd
+++ build.cmd
@@ -1,5 +1,8 @@
 @echo off
 
+rem change console outpul lang to en_US
+chcp 437
+
 rem arg1 - build type
 rem arg2 - version
 set build_type=%1
@@ -10,44 +13,50 @@ if "%build_type%" == "" set build_type=debug
 if "%build_type%" == "Debug" set build_type=debug
 if "%build_type%" == "Release" set build_type=release
 
+rem init environment vars for Visual C
+if not "%DevEnvDir%" == "" goto do_build
+set VCVARS_BAT="c:\Program Files (x86)\Microsoft Visual Studio 10.0\vc\vcvarsall.bat"
+if not exist %VCVARS_BAT% ( set VCVARS_BAT="c:\Program Files\Microsoft Visual Studio 10.0\vc\vcvarsall.bat" )
+call %VCVARS_BAT%
+if %errorlevel% neq 0 goto error
+
+:do_build
 rem clean build folder
 cmake -E remove_directory build
-if not errorlevel 0 goto error
+if %errorlevel% neq 0 goto error
 
 cmake -E make_directory build
-if not errorlevel 0 goto error
+if %errorlevel% neq 0 goto error
 
 cd build
-if not errorlevel 0 goto error
+if %errorlevel% neq 0 goto error
 
 rem configure build
 cmake -G "NMake Makefiles" .. -DCMAKE_BUILD_TYPE=%build_type% -DCMAKE_INSTALL_PREFIX=tmprel -DVERSION=%version%
-if not errorlevel 0 goto error2
+if %errorlevel% neq 0 goto error2
 
 rem run build
 jom all
-if not errorlevel 0 goto error2
+if %errorlevel% neq 0 goto error2
 
 rem run tests
 ctest -V -T Test
-if not errorlevel 0 goto error2
+if %errorlevel% neq 0 goto error2
 
 rem do cpack only for release
 if not "%build_type%" == "release" goto no_error
 
 cpack
-if not errorlevel 0 goto error2
-
-goto no_error
-
-:error2
-set last_error=%errorlevel%
-cd ..
-set errorlevel=%last_error%
-
-:error
-exit /B %errorlevel%
+if %errorlevel% neq 0 goto error2
 
 :no_error
 cd ..
 exit /B 0
+
+:error
+exit /B %errorlevel%
+
+:error2
+set error=%errorlevel%
+cd ..
+exit /B %error%
