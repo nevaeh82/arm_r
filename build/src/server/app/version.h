6ae2e1558f6962c8b64c77c1144fc97e047d52af
Andrey Pavlov <apavlov@stc-spb.ru>
2016-03-29 16:03:52 +0300

    move to qt5 mcvs2013

---
+++ build/src/server/app/version.h
@@ -0,0 +1,21 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VER_FILEVERSION             5,0,0,0
+#define VER_FILEVERSION_STR         "5.0.0\0"
+
+#define VER_PRODUCTVERSION          5,0,0,0
+#define VER_PRODUCTVERSION_STR      "5.0.0\0"
+
+#define VER_COMPANYNAME_STR         "ООО «Специальный технологический центр»"
+#define VER_FILEDESCRIPTION_STR     "Сервер АРМ Р комплекса «Завируха»"
+#define VER_INTERNALNAME_STR        "Zaviruha ARM R Server"
+#define VER_LEGALCOPYRIGHT_STR      "2013-2015 СТЦ"
+#define VER_LEGALTRADEMARKS1_STR    "Все права защищены"
+#define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
+#define VER_ORIGINALFILENAME_STR    "ZaviruhaRServer.exe"
+#define VER_PRODUCTNAME_STR         "Сервер АРМ Р комплекса «Завируха»"
+
+#define VER_COMPANYDOMAIN_STR       "http://www.stc-spb.ru/info.html"
+
+#endif // VERSION_H
